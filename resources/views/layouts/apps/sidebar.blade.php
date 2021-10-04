e25c6cb04b48292000eeab0e0dc3e3f1617a2f32
Hammam Firdaus <hammamxalf@gmail.com>
2021-10-04 07:14:52 +0700

    make sidebar active dinamic

--- resources/views/layouts/apps/sidebar.blade.php
+++ resources/views/layouts/apps/sidebar.blade.php
@@ -3,31 +3,31 @@
         Atendance System
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('dashboard') ? 'active' : '' }}">
         <a class="sidebar-link" href="{{ route('dashboard') }}">
             <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
         </a>
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('rooms') ? 'active' : '' }}">
         <a class="sidebar-link" href="{{ route('rooms.index') }}">
             <i class="align-middle" data-feather="user"></i> <span class="align-middle">Rooms</span>
         </a>
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('courses') ? 'active' : '' }}">
         <a class="sidebar-link" href="{{ route('courses.index') }}">
             <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Courses</span>
         </a>
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('classrooms') ? 'active' : '' }}">
         <a class="sidebar-link" href="{{ route('classrooms.index') }}">
             <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Classrooms</span>
         </a>
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('schedules') ? 'active' : '' }}">
         <a class="sidebar-link" href="{{ route('schedules.index') }}">
             <i class="align-middle" data-feather="book"></i> <span class="align-middle">Schedules</span>
         </a>
@@ -37,13 +37,13 @@
         Learning Management System
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('cek') ? 'active' : '' }}">
         <a class="sidebar-link" href="ui-buttons.html">
             <i class="align-middle" data-feather="square"></i> <span class="align-middle">Topic</span>
         </a>
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('cek') ? 'active' : '' }}">
         <a class="sidebar-link" href="ui-forms.html">
             <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Attachement</span>
         </a>
@@ -53,19 +53,19 @@
         Manage Users
     </li>
 
-    <li class="sidebar-item active">
+    <li class="sidebar-item {{ request()->is('cek') ? 'active' : '' }}">
         <a class="sidebar-link" href="charts-chartjs.html">
             <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Students</span>
         </a>
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('cek') ? 'active' : '' }}">
         <a class="sidebar-link" href="maps-google.html">
             <i class="align-middle" data-feather="map"></i> <span class="align-middle">Teachers</span>
         </a>
     </li>
 
-    <li class="sidebar-item">
+    <li class="sidebar-item {{ request()->is('cek') ? 'active' : '' }}">
         <a class="sidebar-link" href="maps-google.html">
             <i class="align-middle" data-feather="map"></i> <span class="align-middle">Admin</span>
         </a>
