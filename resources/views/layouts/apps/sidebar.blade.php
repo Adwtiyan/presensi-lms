e205313fb910fea8b2acbb5b83f53db1724984f3
Hammam Firdaus <hammamxalf@gmail.com>
2021-10-04 07:09:24 +0700

    adjust(view-layout): change some title sidebar #27

--- resources/views/layouts/apps/sidebar.blade.php
+++ resources/views/layouts/apps/sidebar.blade.php
@@ -1,87 +1,73 @@
 <ul class="sidebar-nav">
     <li class="sidebar-header">
-        Pages
+        Atendance System
     </li>
 
     <li class="sidebar-item">
-        <a class="sidebar-link" href="index.html">
+        <a class="sidebar-link" href="{{ route('dashboard') }}">
             <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
         </a>
     </li>
 
     <li class="sidebar-item">
-        <a class="sidebar-link" href="pages-profile.html">
-            <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+        <a class="sidebar-link" href="{{ route('rooms.index') }}">
+            <i class="align-middle" data-feather="user"></i> <span class="align-middle">Rooms</span>
         </a>
     </li>
 
     <li class="sidebar-item">
-        <a class="sidebar-link" href="pages-sign-in.html">
-            <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Sign
-                In</span>
+        <a class="sidebar-link" href="{{ route('courses.index') }}">
+            <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Courses</span>
         </a>
     </li>
 
     <li class="sidebar-item">
-        <a class="sidebar-link" href="pages-sign-up.html">
-            <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign
-                Up</span>
+        <a class="sidebar-link" href="{{ route('classrooms.index') }}">
+            <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Classrooms</span>
         </a>
     </li>
 
     <li class="sidebar-item">
-        <a class="sidebar-link" href="pages-blank.html">
-            <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
+        <a class="sidebar-link" href="{{ route('schedules.index') }}">
+            <i class="align-middle" data-feather="book"></i> <span class="align-middle">Schedules</span>
         </a>
     </li>
 
     <li class="sidebar-header">
-        Tools & Components
+        Learning Management System
     </li>
 
     <li class="sidebar-item">
         <a class="sidebar-link" href="ui-buttons.html">
-            <i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span>
+            <i class="align-middle" data-feather="square"></i> <span class="align-middle">Topic</span>
         </a>
     </li>
 
     <li class="sidebar-item">
         <a class="sidebar-link" href="ui-forms.html">
-            <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
-        </a>
-    </li>
-
-    <li class="sidebar-item">
-        <a class="sidebar-link" href="ui-cards.html">
-            <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
-        </a>
-    </li>
-
-    <li class="sidebar-item">
-        <a class="sidebar-link" href="ui-typography.html">
-            <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
-        </a>
-    </li>
-
-    <li class="sidebar-item">
-        <a class="sidebar-link" href="icons-feather.html">
-            <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
+            <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Attachement</span>
         </a>
     </li>
 
     <li class="sidebar-header">
-        Plugins & Addons
+        Manage Users
     </li>
 
     <li class="sidebar-item active">
         <a class="sidebar-link" href="charts-chartjs.html">
-            <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
+            <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Students</span>
         </a>
     </li>
 
     <li class="sidebar-item">
         <a class="sidebar-link" href="maps-google.html">
-            <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
+            <i class="align-middle" data-feather="map"></i> <span class="align-middle">Teachers</span>
+        </a>
+    </li>
+
+    <li class="sidebar-item">
+        <a class="sidebar-link" href="maps-google.html">
+            <i class="align-middle" data-feather="map"></i> <span class="align-middle">Admin</span>
         </a>
     </li>
 </ul>
