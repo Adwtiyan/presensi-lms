48121fc54de328e943f89db07f6dbae11ad80ea5
MIKHAEL ALEXANDER ANDY IRAWAN <mikhaelalexanderai@gmail.com>
2021-10-07 11:11:41 +0000

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,69 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<head>
+    @include('includes.css')
+    <link rel="shortcut icon" href="{{ asset('assets/admin-kit/img/logo.jfif') }}" type="image/x-icon">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+    <title>Sign In | Amikom Center</title>
+</head>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<body>
+    <main class="d-flex w-100">
+        <div class="container d-flex flex-column">
+            <div class="row vh-100">
+                <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+                    <div class="d-table-cell align-middle">
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+                        <x-guest-layout>
+                            <x-auth-card>
+                                <x-slot name="logo">
+                                    <a href="{{ asset('assets/admin-kit/img/logo.jfif') }}">
+                                        <img src="{{ asset('assets/admin-kit/img/logo.jfif') }}" alt="Amikom Center"
+                                        class="img-fluid rounded-circle" width="132" height="132" />
+                                    </a>
+                                </x-slot>
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+                                <!-- Validation Errors -->
+                                <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                                <form method="POST" action="{{ route('register') }}">
+                                    @csrf
+
+                                    <!-- Email Address -->
+                                    <div class="mt-4">
+                                        <x-label for="email" :value="__('Email')" />
+
+                                        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                                    </div>
+
+                                    <!-- Password -->
+                                    <div class="mt-4">
+                                        <x-label for="password" :value="__('Password')" />
+
+                                        <x-input id="password" class="block mt-1 w-full"
+                                                        type="password"
+                                                        name="password"
+                                                        required autocomplete="new-password" />
+                                    </div>
+
+                                    <div class="flex items-center justify-end mt-4">
+                                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                                            {{ __('Register') }}
+                                        </a>
+
+                                        <x-button class="ml-4">
+                                            {{ __('Sign In') }}
+                                        </x-button>
+                                    </div>
+                                </form>
+                            </x-auth-card>
+                        </x-guest-layout>
+
+
+                    </div>
+                </div>
             </div>
+        </div>
+    </main>
+    @include('includes.js')
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+</body>
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
