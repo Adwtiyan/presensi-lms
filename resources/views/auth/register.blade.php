70ad6c1062812dae288ad98304bebac0063aca4f
MIKHAEL ALEXANDER ANDY IRAWAN <mikhaelalexanderai@gmail.com>
2021-10-11 13:17:35 +0000

    Update register.blade.php (4)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,63 +1,64 @@
 @extends('layouts.apps.auth')
 <!-- Section Title -->
 @section('title')
-    <title>Sign Up | Amikom Center</title>
+<title>Sign Up | Amikom Center</title>
 @endsection
 
 <!-- Section Form Title -->
 @section('form-title')
-    <h1 class="h2">Sign Up</h1>
-    <p class="lead">
-        to your account to continue
-    </p>
+<h1 class="h2">Sign Up</h1>
+<p class="lead">
+    to your account to continue
+</p>
 @endsection
 
 <!-- Section Form Primary -->
 @section('forms')
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<form method="POST" action="{{ route('register') }}">
+    @csrf
 
-        <!-- Name -->
-        <div class="mb-3">
-            <x-label class="form-label" for="name" :value="__('Name')" />
+    <!-- Name -->
+    <div class="mb-3">
+        <x-label class="form-label" for="name" :value="__('Name')" />
 
-            <x-input id="name" placeholder="Enter your name" class="form-control form-control-lg" type="text" name="name" :value="old('name')" required autofocus />
-        </div>
+        <x-input id="name" placeholder="Enter your name" class="form-control form-control-lg" type="text" name="name"
+            :value="old('name')" required autofocus />
+    </div>
 
-        <!-- Email Address -->
-        <div class="mb-3">
-            <x-label class="form-label" for="email" :value="__('Email')" />
+    <!-- Email Address -->
+    <div class="mb-3">
+        <x-label class="form-label" for="email" :value="__('Email')" />
 
-            <x-input id="email" placeholder="Enter your email" class="form-control form-control-lg" type="email" name="email" :value="old('email')" required />
-        </div>
+        <x-input id="email" placeholder="Enter your email" class="form-control form-control-lg" type="email"
+            name="email" :value="old('email')" required />
+    </div>
 
-        <!-- Password -->
-        <div class="mb-3">
-            <x-label class="form-label" for="password" :value="__('Password')" />
+    <!-- Password -->
+    <div class="mb-3">
+        <x-label class="form-label" for="password" :value="__('Password')" />
 
-            <x-input id="password" placeholder="Enter your password" class="form-control form-control-lg"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-        </div>
+        <x-input id="password" placeholder="Enter your password" class="form-control form-control-lg" type="password"
+            name="password" required autocomplete="new-password" />
+    </div>
 
-        <!-- Confirm Password -->
-        <div class="mb-3">
-            <x-label class="form-label" for="password_confirmation" :value="__('Confirm Password')" />
+    <!-- Confirm Password -->
+    <div class="mb-3">
+        <x-label class="form-label" for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-input id="password_confirmation" placeholder="Confirm your password" class="form-control form-control-lg"
-                            type="password"
-                            name="password_confirmation" required />
-        </div>
-        <div class="text-center mt-3">
-            <button class="btn btn-lg btn-primary">
-                {{ __('Sign up') }}
-            </button>
-        </div>
-    </form>
+        <x-input id="password_confirmation" placeholder="Confirm your password" class="form-control form-control-lg"
+            type="password" name="password_confirmation" required />
+    </div>
+    <div class="text-center mt-3">
+        <button class="btn btn-lg btn-primary">
+            {{ __('Sign up') }}
+        </button>
+    </div>
+</form>
 
-    <div class="text-center mt-4">
-    <center><p>Already have an account? <a href="{{ route('register') }}">Login</a></p></center>
+<div class="text-center mt-4">
+    <center>
+        <p>Already have an account? <a href="{{ _('login') }}">Sign In</a></p>
+    </center>
 </div>
 
 @endsection
